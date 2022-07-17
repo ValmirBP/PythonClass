c5e669e690cf28256809b2853125739179d0f6cd
Valmir <valmir.barros57@yhoo.com.br>
2022-07-17 20:55:35 -0300

    New folder added  to organize the  studies by module

--- module01/challenges/challenge021.py
+++ module01/challenges/challenge021.py
@@ -4,7 +4,7 @@ from emoji import emojize
 print("{:=^50}\n".format(emojize(" CHALLENGE 021 :crossed_swords:  ")))
 
 print(" Write a softare that read an MP3 audio\n")
-audioPath=r"C:\Users\valmi\OneDrive\Documentos\ambiente\estudos\linguagens\Python\PythonClass\challenges\Challenge21.mp3"
+audioPath=r"C:\Users\valmi\OneDrive\Documentos\ambiente\estudos\linguagens\Python\PythonClass\module01\challenges\Challenge21.mp3"
 pygame.mixer.init()
 pygame.mixer.music.load(audioPath)
 pygame.mixer.music.play(loops=0,start=0.0)
