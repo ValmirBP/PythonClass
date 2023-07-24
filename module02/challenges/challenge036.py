67a50f831b7ca7c46ba3bf01382d5d39c2940e6a
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-07-23 21:07:16 -0400

    added a  class color and  added averification  on each input  on chaleenge 36

--- module02/challenges/challenge036.py
+++ module02/challenges/challenge036.py
@@ -3,17 +3,22 @@ from ClassColorsEmojis import *
 
 print("{} {:=^50} {}".format(Color.greenBold, emojize(" CHALLENGE 036" + Emoji.challenge), Color.reset))
 
-print("\nWrite a Software that asks the user for their wage, the value of a house, and the payment schedule.")
-print("""Calculate the value of the monthly payment that does not exceed 30 percent of their wage, as
+print("""Write a Software that asks the user for their wage, the value of a house, and the payment schedule.
+Calculate the value of the monthly payment that does not exceed 30 percent of their wage, as
 it is forbidden to exceed that amount.\n""")
 
-
-wage = float(input("Type the wage: "))
-valueHouse = float(input("Type the value of the house: "))
-period = int(input("Type the period of payment in months: "))
+while  True:
+    try:
+        wage = float(input("Type the wage: "))
+        valueHouse = float(input("Type the value of the house: "))
+        period = int(input("Type the period of payment in months: "))
+        break
+    except ValueError:
+        print("the input is not  correct please try again\n")
 
 percentageWage = (valueHouse / period) * 100 / wage
 idealValue = (valueHouse * 100) / (wage * 30)
+
 
 if percentageWage > 30:
     print("\nSorry! Unfortunately, it is not possible to approve your finance because it exceeds 30% of your wage.\n")
@@ -28,6 +33,6 @@ if percentageWage > 30:
         print("\nOk! See you next time.")
 
 else:
-    print("\nGood! Contact any consultant to sign the proposal.")
+    print("\nGood! Contact any consultant to sign the proposal.\n")
 
 print("{} {:=^50} {}".format(Color.greenBold, emojize("CHALLENGE 036 END " + Emoji.challenge), Color.reset))
