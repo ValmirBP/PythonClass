ac2b74404d04ef131b27d552d34c4380845bbbcb
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-07-27 20:30:59 -0400

    improvement of code

--- module02/challenges/challenge041.py
+++ module02/challenges/challenge041.py
@@ -1,54 +1,45 @@
 from datetime import date
-from tkinter.messagebox import YES
 from emoji import emojize
+from ClassColorsEmojis import *
 
 
+print("{}{:=^50}{}".format(Color.greenBold, emojize("CHALLENGE 041",Emoji.challenge), Color.reset))
 
+print(f"""{Color.red} Write a software that ask to the user the year of his birth and analyse the
+categories of a swimming competition{Color.reset}\n""")
 
-colorDic = {
-    'bold'          : '\033[1m'
-    ,'red'          : '\033[31m'
-    ,'green'        : '\033[32m'
-    ,'yellow'       : '\033[33m'
-    ,'blue'         : '\033[34m'
-    ,'redBold'      : '\033[1;31m'
-    ,'greenBold'    : '\033[1;32m'
-    ,'yellowBold'   : '\033[1;33m'
-    ,'blueBold'     : '\033[1;34m'
-    ,'end'          : '\033[m'
-}
-
-print("{}{:=^50}{}".format(colorDic['greenBold'], emojize("CHALLENGE 041 :crossed_swords:"), colorDic['end']))
-
-print("{}Write a software that ask to the user the year of his birth and analyse the categories of a swimming competition{}\n".format(colorDic['red'], colorDic['end']))
-
-birthYear = int(input("Type here your birth year: ").strip())
+while True:
+    try:
+        birthYear = int(input("Type here your birth year: ").strip())
+        break
+    except ValueError:
+        print(f"{Color.redBold}Invalid input. Please enter a valid year.{Color.reset}")
 
 actualYear= date.today().year
 age = actualYear - birthYear
 
 if age == 9:
     stage = "Little"
-    print("{}You are in category {} {}\n".format(colorDic['green'], stage, colorDic['end']))
+    print(f"{Color.green}You are in category {stage} {Color.reset}\n")
 
 elif age > 9 and age <= 14:
     stage = "childish"
-    print("{}You are in category {} {}\n".format(colorDic['blue'], stage, colorDic['end']))
+    print(f"{Color.blue}You are in category {stage} {Color.reset}\n")
 
 elif age > 14 and age <= 19:
     stage = "Junior"
-    print("{}You are in category {} {}\n".format(colorDic['yellow'], stage, colorDic['end']))
+    print(f"{Color.yellow}You are in category {stage} {Color.reset}\n")
 
 elif age > 19 and age <= 20:
     stage = "Senior"
-    print("{}You are in category {} {}\n".format(colorDic['blueBold'], stage, colorDic['end']))
+    print(f"{Color.blueBold}You are in category {stage} {Color.reset}\n")
 
 elif age > 20:
     stage = "Master"
-    print("{}You are in category {} {}\n".format(colorDic['yellowBold'], stage, colorDic['end']))
+    print(f"{Color.yellowBold}You are in category {stage} {Color.reset}\n")
 
 else:
-    print("{}Sorry You are too young{}".format(colorDic['redBold'], colorDic['end']))
+    print(f"{Color.redBold}Sorry You are too young{Color.reset}")
 
 
-print("{}{:=^50}{}".format(colorDic['greenBold'], emojize("CHALLENGE 041 END :crossed_swords:"), colorDic['end']))
+print("{}{:=^50}{}".format(Color.greenBold, emojize("CHALLENGE 041 END", Emoji.challenge), Color.reset))
