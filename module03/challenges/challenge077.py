ce2ee8580dc3b7137ec4a017494455aee3623062
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-08-29 17:54:01 -0400

    added Challenge 77

---
+++ module03/challenges/challenge077.py
@@ -0,0 +1,11 @@
+from random import randint
+from ClassColorsEmojis import *
+from emoji import emojize
+
+print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 77 " + Emoji.challenge),Color.reset))
+
+print(f"""\n{Color.cyan}
+{Color.reset}\n""")
+
+
+print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 77 END" + Emoji.challenge),Color.reset))
