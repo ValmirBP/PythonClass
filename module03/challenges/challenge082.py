3c8a8ce1ab11fbf16aef7b579758fb44d4944be7
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-09-06 22:24:30 -0400

    Finished Challenge 82  and starded Challenge  83

--- module03/challenges/challenge082.py
+++ module03/challenges/challenge082.py
@@ -3,8 +3,52 @@ from emoji import emojize
 
 print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 " + Emoji.challenge),Color.reset))
 
-print(f"""\n{Color.cyan}
+print(f"""\n{Color.cyan} Create a program that will read several numbers and put them in a list.
+After that, create two extra lists that will contain only the even and odd values entered,
+respectively. At the end, show the contents of the three generated lists.
 {Color.reset}\n""")
+values = []
+oddValues = []
+evenValues = []
+
+def checkInput():
+    while True:
+        try:
+           number =  int(input(f'{Color.greenBold}Enter a number: {Color.reset}'))
+           return number
+        except ValueError:
+            print(f'{Color.redBold}\ninvalid input{Color.reset}')
+
+def askContinue ():
+    while True:
+        ask = input(f'{Color.greenBold}Do you want to continue? [Y]Yes [N]No {Color.reset}').strip().upper()
+        if ask[0] == 'Y':
+            return True
+        if ask[0] == 'N':
+            return False
+        else:
+            print(f'{Color.redBold}\nInvalid input, please enter Y or N{Color.reset}')
+
+while True:
+    number = checkInput()
+    values.append(number)
+    ask = askContinue()
+    if not ask:
+        break
+
+for element in values:
+    if element % 2  == 0:
+        evenValues.append(element)
+    else:
+        oddValues.append(element)
+
+values.sort()
+oddValues.sort()
+evenValues.sort()
+
+print(f'{Color.blueBold}\nThe list of the values is {values} {Color.reset}',end='')
+print(f'{Color.blueBold}\nThe list of the even values is {evenValues} {Color.reset}',end='')
+print(f'{Color.blueBold}\nThe list of the odd values is {oddValues} {Color.reset}',end='')
 
 
 print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 END" + Emoji.challenge),Color.reset))
--- module03/challenges/challenge083.py
+++ module03/challenges/challenge083.py
@@ -3,8 +3,11 @@ from emoji import emojize
 
 print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 " + Emoji.challenge),Color.reset))
 
-print(f"""\n{Color.cyan}
+print(f"""\n{Color.cyan} Create a program where the user types any expression that uses parentheses.
+Your application should analyze whether the passed expression has the open and closed parentheses
+in the correct order.
 {Color.reset}\n""")
+
 
 
 print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 END" + Emoji.challenge),Color.reset))
