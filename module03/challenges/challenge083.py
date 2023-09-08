4229b27d753826d4718b6d42e607fc1d2141aa78
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-09-07 20:58:28 -0400

    finished Challenge 80

--- module03/challenges/challenge083.py
+++ module03/challenges/challenge083.py
@@ -8,6 +8,21 @@ Your application should analyze whether the passed expression has the open and c
 in the correct order.
 {Color.reset}\n""")
 
+expression = str(input(f'Type here the mathematic expression: '))
 
+pile =[]
+for symbol in expression:
+    if symbol == '(':
+        pile.append('(')
+    elif symbol == ')':
+        if len(pile) > 0:
+            pile.pop()
+        else:
+            pile.append(')')
+            break
+if len(pile) == 0:
+    print(f'{Color.greenBold}Math expression right{Color.reset}')
+else:
+    print(f'{Color.red}Math expression wrong{Color.reset}')
 
 print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 END" + Emoji.challenge),Color.reset))
