9699e96a65b3c0a5644b6d518d2eebf91a0aa3c0
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-09-10 15:23:54 -0400

    improved git ignore, finished  Challenge  87,88

--- module03/challenges/challenge087.py
+++ module03/challenges/challenge087.py
@@ -1,10 +1,57 @@
 from ClassColorsEmojis import *
 from emoji import emojize
 
-print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 " + Emoji.challenge),Color.reset))
+print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 87 " + Emoji.challenge),Color.reset))
 
-print(f"""\n{Color.cyan}
+print(f"""\n{Color.cyan} Improve the previous challenge by showing at the end:
+A) The sum of all even values entered.
+B) The sum of the values in the third column.
+C) The largest value in the second line.
 {Color.reset}\n""")
 
 
-print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 END" + Emoji.challenge),Color.reset))
+Matrix = [[0,0,0],[0,0,0],[0,0,0]]
+evenSum = thirdCol = biggest = 0
+
+def checkInput():
+    while True:
+        try:
+            uInput = int(input(f'{Color.greenBold}Enter a number{Color.reset}: '))
+            return uInput
+        except ValueError:
+            print(f'{Color.redBold}\nInvalid input.{Color.reset}')
+
+for l in range(3): # built the Matrix
+    for c in range(3):
+        Matrix[l] [c] = checkInput()
+
+print()
+
+for l in range(3): # Format Matrix
+    for c in range(3):
+        print (f'{Color.yellowBold} [{Matrix [l] [c]}] {Color.reset}',end='')
+    print()
+
+print()
+
+for l in range(3): # Sum all even numbers in Matrix
+    for c in range(3):
+        if  Matrix[l][c] % 2 == 0:
+            evenSum += Matrix[l] [c]
+
+print(f'{Color.greenBold}The sum of all even number is{Color.blue} {evenSum} {Color.reset}')
+
+for l in range(3): # make the sum of numbers on third colum
+    thirdCol += Matrix[l][2]
+
+print(f'{Color.greenBold}The sum of numbers of third column is {Color.blue} {thirdCol} {Color.reset}')
+
+for c in range(3):
+    if c == 0:
+        biggest = Matrix[1][c]
+    elif Matrix[1][c] > biggest:
+        biggest = Matrix[1][c]
+
+print(f'{Color.greenBold}The sum of numbers of third column is{Color.blue} {biggest} {Color.reset}')
+
+print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 87 END" + Emoji.challenge),Color.reset))
--- module03/challenges/challenge088.py
+++ module03/challenges/challenge088.py
@@ -1,10 +1,45 @@
+from random import randint
 from ClassColorsEmojis import *
 from emoji import emojize
 
-print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 " + Emoji.challenge),Color.reset))
+print ("{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 88 " + Emoji.challenge),Color.reset))
 
-print(f"""\n{Color.cyan}
+print(f"""\n{Color.cyan}Write a program that helps a LOTO MAX player create predictions.
+The program will ask how many games will be generated and will draw 6 numbers
+between 1 and 60 for each game, registering everything in a composite list.
 {Color.reset}\n""")
 
+games = list()
+singleGame = list()
 
-print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 80 END" + Emoji.challenge),Color.reset))
+def CheckImput():
+    while True:
+        try:
+            uinput = int(input(f'{Color.green}Enter how many games you want to play {Color.reset}'))
+            return uinput
+        except ValueError:
+            print(f'{Color.redBold}Invalid imput {Color.reset}')
+
+def gameGen():
+    count = 0
+    while True:
+        num = randint (1,60)
+        if num not in singleGame:
+            singleGame.append(num)
+            count+=1
+        if count >= 6 :
+            games.append(singleGame[:])
+            singleGame.clear()
+            break
+
+numGames = CheckImput()
+
+for game in range(numGames):
+    gameGen()
+
+print(f'{Color.green}The games are: \n{Color.reset}')
+
+for game in range(len(games)):
+    print(f'{Color.yellowBold} {games[game]} {Color.reset}')
+
+print ("\n{}{:=^50}{}".format(Color.redBold,emojize("CHALLENGE 88 END" + Emoji.challenge),Color.reset))
