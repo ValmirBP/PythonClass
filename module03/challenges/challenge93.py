edb672ee0612d1cba282c937a29c267339119606
valmir_barros <vbarros@screamingpower.com>
2024-01-16 12:57:10 -0500

    Refactor code to improve readability and organization.
    - Rename variables to be more descriptive.
    - Use a loop to collect game information instead of hard-coding it.
    - Print information line by line for better formatting.

--- module03/challenges/challenge93.py
+++ module03/challenges/challenge93.py
@@ -9,28 +9,40 @@ print(f"""\n{Color.cyan} Create a program that manages the performance of a foot
       stored in a dictionary, including the total number of goals scored during the championship.
 {Color.reset}\n""")
 
-playerName = input(f'{Color.lightBlue}gamer name{Color.reset}')
-while True:
-    try:
-        games = int(input(f'{Color.lightBlue} games played{Color.reset}'))
-        break
-    except ValueError:
-        print(f'{Color.red}invalid input!!!{Color.reset}')
-
+playerName = input(f'{Color.lightBlue}Enter the player name: {Color.reset}')
 championship = []
 goals = []
-game = 0
-while game < games:
-    goal = int(input(f'number of goals on game {game}: '))
-    game += 1
+game = 1  
+
+while True:
+    try:
+        games = int(input(f'{Color.lightBlue}Enter the games played: {Color.reset}'))
+        break
+    except ValueError:
+        print(f'{Color.red}Invalid input!!!{Color.reset}')
+
+while game <= games:
+    goal = int(input(f'Number of goals on game {game}: '))
     goals.append(goal)
+    game += 1
 
 gamesInfo = {
-        'Name': playerName,
-        'games': games,
-        'goals': goals
-    }
+    'Name': playerName,
+    'Games': games,
+    'Goals': {f'Game {i + 1}': goals[i] for i in range(len(goals))},
+    'Total Goals': sum(goals)
+}
+
 championship.append(gamesInfo)
-print(championship)
+
+# Print information line by line
+print(f"\n{Color.yellow}Championship Information:{Color.reset}")
+for info in championship:
+    print(f"{Color.lightBlue}Player Name: {info['Name']}{Color.reset}")
+    print(f"{Color.lightBlue}Games Played: {info['Games']}{Color.reset}")
+    print(f"{Color.lightBlue}Goals by Game:")
+    for game, goals in info['Goals'].items():
+        print(f"  {game}: {goals} goals")
+    print(f"{Color.lightBlue}Total Goals: {info['Total Goals']}{Color.reset}")
 
 print("\n{}{:=^50}{}".format(Color.redBold, emojize("CHALLENGE 93 END" + Emoji.challenge), Color.reset))
